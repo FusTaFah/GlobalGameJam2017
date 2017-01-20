c6ce2d7173ce2080d6b62ed033af5ec663888c62
FusTaFah <mfadel_1990@hotmail.com>
2017-01-20 18:57:21 +0000

    network up and running

--- Assets/Scripts/NetworkStartup.cs
+++ Assets/Scripts/NetworkStartup.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworkStartup : MonoBehaviour {
+public class NetworkStartup : Photon.MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    public void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        foreach (object error in codeAndMsg)
+        {
+            Debug.Log(error);
+        }
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public void OnJoinedRoom()
+    {
+        Debug.Log("Connected to room");
+        GameObject player = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+        player.transform.position = new Vector3(-3.5702f, 0.15367f, -3.35f);
+    }
 }
